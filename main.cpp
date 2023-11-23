89845ec25899dab51d76b0e0ba40f40607e2c13c
eristaufiqhidayat <115045433+eristaufiqhidayat@users.noreply.github.com>
2023-11-23 08:48:00 +0700

    first time

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+#include <mysql.h>
+#include <mysqld_error.h>
+
+using namespace std;
+
+char HOST[]="localhost";
+char USER[]="root";
+char PASS[]="";
+char DB[]="kasir";
+
+MYSQL *obj;
+MYSQL *conection;
+MYSQL_RES *result;
+MYSQL_DATA data;
+MYSQL_ROW row;
+
+int main(int argc, char *argv[])
+{
+    cout << "Press the enter key to continue ...";
+    cin.get();
+    return EXIT_SUCCESS;
+}
