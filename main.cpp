9eb44be7c787d9a88726e4063ae468edbba6ae06
eristaufiqhidayat <115045433+eristaufiqhidayat@users.noreply.github.com>
2023-11-23 09:20:06 +0700

    1

--- main.cpp
+++ main.cpp
@@ -2,23 +2,51 @@
 #include <iostream>
 #include <mysql.h>
 #include <mysqld_error.h>
-
 using namespace std;
 
-char HOST[] = "localhost";
-char USER[] = "root";
-char PASS[] = "";
-char DB[] = "dana_osis";
-
+char HOST[]="localhost";
+char USER[]="root";
+char PASS[]="";
+char DB[]="dana_osis";
 MYSQL *obj;
 MYSQL *conection;
 MYSQL_RES *result;
 MYSQL_DATA data;
 MYSQL_ROW row;
 
-int main(int argc, char *argv[])
+using namespace std;
+
+void tambahdata(){
+    if(!(obj = mysql_init(0))){
+         cout <<"error";   
+    }else{
+        conection = mysql_real_connect(obj,HOST,USER,PASS,DB,3306,NULL,0);
+        mysql_query(obj,"INSERT INTO `siswa` (`id`, `nis`, `nama_depan`, `nama_keluarga`, `tgl_lahir`, `alamat`, `telp`) VALUES (NULL, '1', '2', '3', '4', '5', '6');");
+        result = mysql_use_result(obj);
+        mysql_close(obj);
+    } 
+}
+void selectdata(){
+    if(!(obj = mysql_init(0))){
+         cout <<"error";   
+    }else{
+        conection = mysql_real_connect(obj,HOST,USER,PASS,DB,3306,NULL,0);
+        mysql_query(obj,"select * from siswa");
+        result = mysql_use_result(obj);
+        while(row = mysql_fetch_row(result)){
+           cout <<row[1]<<endl; 
+        }
+        mysql_close(obj);
+    }
+}
+int main()
 {
-    cout << "Press the enter key to continue ...";
-    cin.get();
-    return EXIT_SUCCESS;
+    int tambah;  
+    selectdata();
+    cout << "Tambah Data ?";
+    cin >>tambah;
+    if(tambah == 1){
+        tambahdata();
+    }
+    system("PAUSE");
 }
